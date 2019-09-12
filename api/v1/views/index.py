0d645e32d1669cf87cb37ec7ab4826b23b7dd9fc
RodrigoSierraV <735@holbertonschool.com>
2019-09-12 01:21:40 +0000

    [Add] Feature to retrieve the number of the objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,25 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models import State
+from models import Amenity
+from models import Place
+from models import City
+from models import Review
+from models import User
 
-
+Classes = [State, Amenity, Place, City, Review, User]
 @app_views.route('/status', strict_slashes=False)
 def apistat():
     """Returns JSON with status of the API"""
     return jsonify(status='OK')
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def count():
+    """retrieves the number of each objects by type"""
+
+    dic = {}
+    for i in classes:
+        dic[i.__name__] = storage.count(i.__name__)
+    return dic

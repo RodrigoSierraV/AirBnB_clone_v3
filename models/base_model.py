d197313164ddb996da0676b21cbcd7cf2e495e97
RodrigoSierraV <735@holbertonschool.com>
2019-09-13 04:47:25 +0000

    [Add] hash MD5 to user password

--- models/base_model.py
+++ models/base_model.py
@@ -77,7 +77,7 @@ class BaseModel:
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         if getenv('HBNB_TYPE_STORAGE') == 'db':
-            if 'password' in new_dict.keys():
+            if 'password' in new_dict:
                 del new_dict['password']
         return new_dict
 
